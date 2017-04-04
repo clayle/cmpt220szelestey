3b5c5ccbdc53e62853b831a48729b213fc1499ab
Clayton Szelestey <Clayton.Szelestey1@marist.edu>
2017-04-04 19:21:43 -0400

    Clayton Szelestey, Lab 9

---
+++ LABS/9/Problem12_13.java
@@ -0,0 +1,45 @@
+//Clayton Szelestey
+//Software Development
+//Lab 9
+import java.io.*;
+import java.util.*;
+
+public class Problem12_13 {
+
+	public static void main(String[] args) throws Exception {
+		if (args.length != 1) {
+			System.out.println("Usage: java filename");
+			System.exit(1);
+		}	
+
+		File file = new File(args[0]);
+		if (!file.exists()) {
+			System.out.println("File " + args[0] + " does not exist");
+			System.exit(2);
+		}
+
+		int characters = 0;	
+		int words = 0;		
+		int lines = 0;		
+
+		try (Scanner input = new Scanner(file);) {
+			while (input.hasNextLine()) {
+				lines++;
+				String line = input.nextLine();
+				characters += line.length();
+			}
+		}
+
+		try (Scanner input = new Scanner(file);) {
+			while (input.hasNext()) {
+				String line = input.next();
+				words++;
+			}
+		}
+
+		System.out.println("File " + file.getName() + " has");
+		System.out.println(characters + " characters");
+		System.out.println(words + " words");
+		System.out.println(lines + " lines");
+	}
+}
